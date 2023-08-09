73e14bcdd05c6cda92f190c7fbd8064753807c0b
yogendra1109 <73202364+yogendra1109@users.noreply.github.com>
2023-08-09 22:36:30 +0530

    Update client_test.py

--- client_test.py
+++ client_test.py
@@ -7,6 +7,16 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
+            # Test calculations for each quote
+        for quote in quotes:
+            stock, bid_price, ask_price, price = getDataPoint(quote)
+            
+            # Assert expected values based on the provided quote data
+            self.assertEqual(stock, quote['stock'])
+            self.assertEqual(bid_price, float(quote['top_bid']['price']))
+            self.assertEqual(ask_price, float(quote['top_ask']['price']))
+            self.assertEqual(price, (float(quote['top_bid']['price']) + float(quote['top_ask']['price'])) / 2)
+
     """ ------------ Add the assertion below ------------ """
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
@@ -14,8 +24,25 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
+      # Test calculations for each quote
+        for quote in quotes:
+            stock, bid_price, ask_price, price = getDataPoint(quote)
+            
+            # Assert bid price is greater than ask price
+            self.assertGreater(bid_price, ask_price)
     """ ------------ Add the assertion below ------------ """
 
+   def test_getDataPoint_zeroAskPrice(self):
+        quote = {'top_ask': {'price': 0, 'size': 0}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'XYZ'}
+        
+        # Test calculation when ask price is 0
+        stock, bid_price, ask_price, price = getDataPoint(quote)
+        
+        # Assert expected values based on the provided quote data
+        self.assertEqual(stock, quote['stock'])
+        self.assertEqual(bid_price, float(quote['top_bid']['price']))
+        self.assertEqual(ask_price, float(quote['top_ask']['price']))
+        self.assertEqual(price, (float(quote['top_bid']['price']) + float(quote['top_ask']['price'])) / 2)
 
   """ ------------ Add more unit tests ------------ """
 
